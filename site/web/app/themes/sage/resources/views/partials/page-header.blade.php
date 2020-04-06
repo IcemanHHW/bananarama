68f2a14d442114af3842ca60cab10514d91847bf
Maarten <maartenkampmeijer@hotmail.com>
2020-04-06 15:43:04 +0200

    version 0.5.1
    changed header text color

--- site/web/app/themes/sage/resources/views/partials/page-header.blade.php
+++ site/web/app/themes/sage/resources/views/partials/page-header.blade.php
@@ -1,6 +1,6 @@
 <div class="hero is-medium">
   <div class="hero-body">
-    <h1 class="title is-1">{!! App::title() !!}</h1>
-    <p class="subtitle is-2">Subtitle</p>
+    <h1 class="title is-1 has-text-white">{!! App::title() !!}</h1>
+    <p class="subtitle is-2 has-text-white">Subtitle</p>
   </div>
 </div>
