7bb0c7230c36852212c5eabcdcfbc139107d794c
IcemanHHW <maartenkampmeijer@hotmail.com>
2020-04-08 17:09:19 +0200

    version 0.5.3.1
    - changed ACF product fields for homepage

--- site/web/app/themes/sage/app/Controllers/FrontPage.php
+++ site/web/app/themes/sage/app/Controllers/FrontPage.php
@@ -13,43 +13,43 @@ class FrontPage extends Controller
 
     /* Products */
     public function hp_product_image_1() {
-        return get_field('product_image_1');
+        return get_field('hp_product_image_1');
     }
 
     public function hp_product_image_2() {
-        return get_field('product_image_2');
+        return get_field('hp_product_image_2');
     }
 
     public function hp_product_image_3() {
-        return get_field('product_image_3');
+        return get_field('hp_product_image_3');
     }
 
     public function hp_product_image_4() {
-        return get_field('product_image_4');
+        return get_field('hp_product_image_4');
     }   
 
     public function hp_product_image_5() {
-        return get_field('product_image_5');
+        return get_field('hp_product_image_5');
     }
 
     public function hp_product_title_1() {
-        return get_field('product_title_1');
+        return get_field('hp_product_title_1');
     }
 
     public function hp_product_title_2() {
-        return get_field('product_title_2');
+        return get_field('hp_product_title_2');
     }
 
     public function hp_product_title_3() {
-        return get_field('product_title_3');
+        return get_field('hp_product_title_3');
     }
 
     public function hp_product_title_4() {
-        return get_field('product_title_4');
+        return get_field('hp_product_title_4');
     }
 
     public function hp_product_title_5() {
-        return get_field('product_title_5');
+        return get_field('hp_product_title_5');
     }
 
     /* About */
